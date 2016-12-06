16dc8678b837fd84ad40b371647dfee5de0c162d
Jake Whittington <jwhitt2@gmail.com>
2016-12-06 18:03:48 -0500

    add lifecycle variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,21 @@ variable "bucket_name" {}
 variable "bucket_acl" {
   default = "private"
 }
+variable "use_lifecycles" {
+  default = false
+}
+variable "lifecycle_id" {
+  default = ""
+}
+variable "lifecycle_prefix" {
+  default = ""
+}
+variable "transition_days" {
+  default = 0
+}
+variable "transition_class" {
+  default = "GLACIER"
+}
+variable "expiration_days" {
+  default = 0
+}
