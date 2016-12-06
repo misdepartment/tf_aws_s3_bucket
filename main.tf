041e2153eb8602ddf34776d6f7e37b644e9c3ab4
Jake Whittington <jwhitt2@gmail.com>
2016-12-06 17:57:07 -0500

    comments

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# Terraform module for creating an S3 bucket.
+
+# See https://www.terraform.io/docs/providers/aws/r/s3_bucket.html
 resource "aws_s3_bucket" "bucket" {
     bucket = "${var.bucket_name}"
     acl = "${var.bucket_acl}"
